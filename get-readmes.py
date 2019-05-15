06242a36d602824c3878a33db7f1e8625406cc4f
Kevin Putnam <kevin.putnam@intel.com>
2019-05-15 10:03:55 -0700

    Added python script for pulling repos and building docs from readmes. Added plugin-list that contains each of the target repos. Updated .gitignore.
    Signed-off-by: Kevin Putnam <kevin.putnam@intel.com>

---
+++ get-readmes.py
@@ -0,0 +1,28 @@
+import os
+
+file = 'plugin-list'
+gitPrefix = 'git@github.com:intel/'
+gitSuffix = '.git'
+absPath = os.path.abspath(__file__)
+homeDir = os.path.dirname(absPath)
+numFiles = 0
+
+
+with open(file) as f:
+    plugins = f.readlines()
+
+for line in plugins:
+    args = line.split()
+    gitDir = args.pop(0)
+    #os.system('git clone ' + gitPrefix + gitDir + gitSuffix)
+
+    for readmePath in args:
+        os.chdir(os.path.dirname(gitDir + "/" + readmePath))
+        outputFileName = 'readme' + str(numFiles) + '.docx'
+        os.system('pandoc ' + os.path.basename(readmePath) + ' -o ' + outputFileName)
+        os.system('mv ' + outputFileName + ' ' + homeDir)
+        os.chdir(homeDir)
+
+        numFiles += 1
+
+os.system('pandoc readme*.docx -o final.docx')
